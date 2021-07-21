df05f7f6e39a3903755bd7d6ffaf97855eb7a243
박지호 <businessj1ho@gmail.com>
2021-07-21 10:33:47 +0900

    main- 15.6 Login Mutation part One

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,57 +1,71 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+
+const LOGIN_MUTATION = gql`
+  mutation PotatoMutation($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      ok
+      token
+      error
+    }
+  }
+`;
 
 interface ILoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 export const Login = () => {
-    const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
-    const onSubmit = () => {};
-  
-    return (
-        <div className="h-screen flex items-center justify-center bg-gray-800">
+  const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+  const [loginMutation] = useMutation(LOGIN_MUTATION);
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        email,
+        password: 1234567890,
+      },
+    });
+  };
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
       <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-            <h3 className="text-2xl text-gray-800">로그인</h3>
-            <form
+        <h3 className="text-2xl text-gray-800">로그인</h3>
+        <form
           onSubmit={handleSubmit(onSubmit)}
           className="grid gap-3 mt-5 px-5"
         >
-              <input
-            ref={register({ required: "이메일을 입력해주세요." })}
+          <input
+            ref={register({ required: "이메일을 입력해 주세요." })}
             name="email"
             required
             type="email"
-            placeholder="이메일"
+            placeholder="Email"
             className="input"
-              />
-              {errors.email?.message && (
-            <span className="font-medium text-red-500">
-              {errors.email?.message}
-            </span>
+          />
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email?.message} />
           )}
-              <input
-            ref={register({ required: "비밀번호를 입력해주세요.", minLength: 10 })}
+          <input
+            ref={register({ required: "패스워드를 입력해 주세요." })}
             required
             name="password"
             type="password"
-            placeholder="비밀번호"
+            placeholder="Password"
             className="input"
-              />
+          />
           {errors.password?.message && (
-            <span className="font-medium text-red-500">
-              {errors.password?.message}
-            </span>
+            <FormError errorMessage={errors.password?.message} />
           )}
           {errors.password?.type === "minLength" && (
-            <span className="font-medium text-red-500">
-              비밀번호는 10자 이상이어야 합니다.
-            </span>
+            <FormError errorMessage="비밀번호는 10자 이상 입력하어야 합니다." />
           )}
           <button className="mt-3 btn">로그인</button>
-            </form>
-          </div>
-        </div>
-        );
+        </form>
+      </div>
+    </div>
+  );
 };
