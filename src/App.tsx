420ee7c92924e5400e8741420ee9ff911664adb2
박지호 <businessj1ho@gmail.com>
2021-07-23 09:52:19 +0900

    main- 17.3 Restaurants Pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useReactiveVar } from "@apollo/client";
 import React from "react";
 import { isLoggedInVar } from "./apollo";
+import { Restaurants } from "./pages/client/restaurants";
 import { LoggedInRouter } from "./router/logged-in-router";
 import { LoggedOutRouter } from "./router/logged-out-router";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
+  return isLoggedIn ? <Restaurants /> : <LoggedOutRouter />;
   }
 
 export default App;
