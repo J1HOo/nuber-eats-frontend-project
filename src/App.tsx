d841ffde32e6e4e36c1f1354288364e004c7ab7f
박지호 <businessj1ho@gmail.com>
2021-07-23 10:44:06 +0900

    main- 17.5 17.6 Category

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useReactiveVar } from "@apollo/client";
 import React from "react";
 import { isLoggedInVar } from "./apollo";
+import { Category } from "./pages/client/category";
 import { Restaurants } from "./pages/client/restaurants";
 import { LoggedInRouter } from "./router/logged-in-router";
 import { LoggedOutRouter } from "./router/logged-out-router";
